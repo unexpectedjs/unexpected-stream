b9f5b5277310ab76f63e20da077bda684d7a4aae
Andreas Lind <andreas@one.com>
2015-10-08 21:43:45 +0200

    Use a fixed width for the output snippets on the documentation site.

--- generate-site.js
+++ generate-site.js
@@ -3,6 +3,7 @@ var argv = require('minimist')(process.argv.slice(2));
 
 global.require = require;
 var unexpected = require('unexpected').clone().installPlugin(require('./lib/unexpectedStream'));
+unexpected.output.preferredWidth = 80;
 var generator = require('unexpected-documentation-site-generator');
 argv.unexpected = unexpected;
 generator(argv);
