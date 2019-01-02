0958de5cacbf843758300933a172953ebcc43e1b
Andreas Lind <andreaslindpetersen@gmail.com>
2019-01-02 23:46:33 +0100

    bootstrap-unexpected-markdown: Clone before installing plugin

--- bootstrap-unexpected-markdown.js
+++ bootstrap-unexpected-markdown.js
@@ -1,4 +1,5 @@
 /*global unexpected:true*/
 unexpected = require('unexpected');
 unexpected.output.preferredWidth = 80;
+unexpected = unexpected.clone();
 unexpected.use(require('./lib/unexpectedStream'));
